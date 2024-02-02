4da4952a960ba36d249b5e57f71d24fbef596660
isuchart <suchart2005@gmail.com>
2024-02-02 21:01:46 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
-let room = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
-    room += '<option value="ม.4">ม.4</option>'
-    room += '<option value="ม.5">ม.5</option>'
+let level = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
+    level += '<option value="ม.4">ม.4</option>'
+    level += '<option value="ม.5">ม.5</option>'
+    level += '<option value="ม.5">ม.5</option>'
 
 $item1 = $("#item1")
-$item1.append(room)
+$item1.append(level)
