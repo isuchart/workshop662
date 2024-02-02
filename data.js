a24e7e6892ce9234d750ee3d3b644000faac50b7
isuchart <suchart2005@gmail.com>
2024-02-02 22:45:18 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,38 @@
-// let level = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
-//     level += '<option value="ม.4">ม.4</option>'
-//     level += '<option value="ม.5">ม.5</option>'
+let level = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
+    level += '<option value="ม.4">ม.4</option>'
+    level += '<option value="ม.5">ม.5</option>'
 
-// let room = '<option value="#" selected disabled>เลือกห้อง</option>'
-//     room += '<option value="8">8</option>'
-//     room += '<option value="10">10</option>'
-
-// $item1 = $("#item1")
-// $item1.append(level)
-
-// $item2 = $("#item2")
-// $item1.on("change", function()  {
-//    $item2.append(room)
-// })
-
-let levelOptions = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
-    levelOptions += '<option value="ม.4">ม.4</option>'
-    levelOptions += '<option value="ม.5">ม.5</option>'
-
-let roomOptions = '<option value="#" selected disabled>เลือกห้อง</option>'
-    roomOptions += '<option value="8">8</option>'
-    roomOptions += '<option value="10">10</option>'
+let room = '<option value="#" selected disabled>เลือกห้อง</option>'
+    room += '<option value="8">8</option>'
+    room += '<option value="10">10</option>'
 
 $item1 = $("#item1")
-$item1.append(levelOptions)
+$item1.append(level)
 
 $item2 = $("#item2")
 $item1.on("change", function()  {
-   // เคลียร์ option เก่าทุกครั้งที่มีการเปลี่ยนแปลง
    $item2.empty();
-   // ใช้ .append() เพื่อเพิ่ม option ใหม่ลงใน #item2
-   $item2.append(roomOptions);
+   $item2.append(room)
 })
+
+// chatGPT
+
+// let levelOptions = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
+//     levelOptions += '<option value="ม.4">ม.4</option>'
+//     levelOptions += '<option value="ม.5">ม.5</option>'
+
+// let roomOptions = '<option value="#" selected disabled>เลือกห้อง</option>'
+//     roomOptions += '<option value="8">8</option>'
+//     roomOptions += '<option value="10">10</option>'
+
+// $item1 = $("#item1")
+// $item1.append(levelOptions)
+
+// $item2 = $("#item2")
+// $item1.on("change", function()  {
+//    // เคลียร์ option เก่าทุกครั้งที่มีการเปลี่ยนแปลง
+//    $item2.empty();
+//    // ใช้ .append() เพื่อเพิ่ม option ใหม่ลงใน #item2
+//    $item2.append(roomOptions);
+// })
 
