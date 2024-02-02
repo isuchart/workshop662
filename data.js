5a1c5e4a262f5ce4d5f2395224ffa9b7d2586539
isuchart <suchart2005@gmail.com>
2024-02-02 20:38:18 +0700

    แ

--- data.js
+++ data.js
@@ -1,9 +1,6 @@
 let room = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
-   //  room += '<option value="ม.4/8">ม.4/8</option>'
-   //  room += '<option value="ม.4/10">ม.4/10</option>'
-    room += '<option value="ม.5/6">ม.5/6</option>'
-    room += '<option value="ม.5/7">ม.5/7</option>'
-    room += '<option value="ม.5/8">ม.5/8</option>'
-    room += '<option value="ม.5/9">ม.5/9</option>'
+    room += '<option value="ม.4">ม.4</option>'
+    room += '<option value="ม.5">ม.5</option>'
+
 $item1 = $("#item1")
 $item1.append(room)
