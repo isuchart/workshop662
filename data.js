55730272e1e5382be48788ef409aef8417eab97d
isuchart <suchart2005@gmail.com>
2024-02-02 20:04:15 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 let room = '<option value="#" selected disabled>เลือกระดับชั้น</option>'
-    room += '<option value="ม.4/8">ม.4/8</option>'
-    room += '<option value="ม.4/10">ม.4/10</option>'
+   //  room += '<option value="ม.4/8">ม.4/8</option>'
+   //  room += '<option value="ม.4/10">ม.4/10</option>'
     room += '<option value="ม.5/6">ม.5/6</option>'
     room += '<option value="ม.5/7">ม.5/7</option>'
     room += '<option value="ม.5/8">ม.5/8</option>'
